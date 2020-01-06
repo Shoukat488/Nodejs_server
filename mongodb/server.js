08a625163382eb712dd151d6b1e1e4986f9287b6
Shoukat Ali <shoukatali488@gmail.com>
2020-01-06 17:51:30 +0500

    deleted

--- mongodb/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app')
-const http = require('http');
-const port = 3001
-const server = http.createServer(app);
-
-server.listen(port , ()=>{
-    console.log("Connected!")
-});
